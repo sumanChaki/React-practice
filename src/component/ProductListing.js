1557d1d887b1caa09f73951903ae96eb72572e5d
Sandip Nag <sandipnag@Sandips-MacBook-Pro.local>
2023-12-27 01:05:14 +0530

    selected product list code added

--- src/component/ProductListing.js
+++ src/component/ProductListing.js
@@ -1,70 +1,107 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ProductListing = () => {
 
-const [productInfo, setProductInfo] = useState([
-  {
-    id: 1,
-    productName: "Samsung",
-    modelName: "XAVA",
-    category: "Mobile",
-    price: 25000,
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 2,
-    productName: "MI",
-    modelName: "ZOOM",
-    category: "Mobile",
-    price: 20000,
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 3,
-    productName: "Sony",
-    modelName: "Bravia",
-    category: "TV",
-    price: 35000,
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 4,
-    productName: "Panasonic",
-    modelName: "XYLO",
-    category: "TV",
-    price: 40000,
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 5,
-    productName: "Vivo",
-    modelName: "XLM",
-    category: "Mobile",
-    price: 35000,
-    image: "https://picsum.photos/200",
-  },
-]);
+  const [productInfo, setProductInfo] = useState([
+    {
+      id: 1,
+      productName: "Samsung",
+      modelName: "XAVA",
+      category: "Mobile",
+      price: 25000,
+      image: "https://picsum.photos/200",
+    },
+    {
+      id: 2,
+      productName: "MI",
+      modelName: "ZOOM",
+      category: "Mobile",
+      price: 20000,
+      image: "https://picsum.photos/200",
+    },
+    {
+      id: 3,
+      productName: "Sony",
+      modelName: "Bravia",
+      category: "TV",
+      price: 35000,
+      image: "https://picsum.photos/200",
+    },
+    {
+      id: 4,
+      productName: "Panasonic",
+      modelName: "XYLO",
+      category: "TV",
+      price: 40000,
+      image: "https://picsum.photos/200",
+    },
+    {
+      id: 5,
+      productName: "Vivo",
+      modelName: "XLM",
+      category: "Mobile",
+      price: 35000,
+      image: "https://picsum.photos/200",
+    },
+  ]);
 
-const selectUser = (userId) => {
-  setProductInfo(prev => {
-    return prev.map(productUser => {
-      return productUser.id == userId ? {...productUser, selected: true} : {...productUser}
+  const selectUser = (userId) => {
+    setProductInfo(prev => {
+      return prev.map(productUser => {
+        return productUser.id == userId ? { ...productUser, selected: true } : { ...productUser }
+      })
     })
-  })
-}
+  }
 
-const priceFilterHandler = () => {
-  setProductInfo((prev) =>
-    prev.filter((value) => (value.price >= 30000 ? { ...value } : ""))
-  );
-};
+  const priceFilterHandler = () => {
+    setProductInfo((prev) =>
+      prev.filter((value) => (value.price >= 30000 ? { ...value } : ""))
+    );
+  };
+
+
+  useEffect(() => {
+    console.log("productInfo =>", productInfo);
+  }, [productInfo])
 
   return (
     <>
       <div className="new-product-wrapper">
         <h2>Selected Product Items by Sandip</h2>
         <div className="product-listing-wrapper">
-          {productInfo.map((productItem, index) => {
+          {productInfo.filter((single, index) => (single.hasOwnProperty("selected") && single.selected == true)).map((productItem, index) => {
+            let { id, productName, modelName, category, price, image } =
+              productItem;
+            return (
+              <div
+                className={`product-listing`}
+                key={index}
+              >
+                <figure className="product-image">
+                  <img src={image} />
+                </figure>
+                <h3>
+                  <span>Product Name:</span>
+                  {productName}
+                </h3>
+                <div className="product-wrap">
+                  <h4>
+                    <span>Model Name:</span>
+                    {modelName}
+                  </h4>
+                  <h4>
+                    <span>Category:</span>
+                    {category}
+                  </h4>
+                </div>
+                <div className="price-text">
+                  <span>Price</span>
+                  {price}
+                </div>
+              </div>
+            )
+          })}
+          {/* {productInfo.map((productItem, index) => {
             let { id, productName, modelName, category, price, image } =
               productItem;
 
@@ -100,7 +137,7 @@ const priceFilterHandler = () => {
                 </div>
               </div>
             );
-          })}
+          })} */}
         </div>
       </div>
 
@@ -111,11 +148,10 @@ const priceFilterHandler = () => {
             productUser;
           return (
             <div
-              className={`product-listing ${
-                productUser.hasOwnProperty("selected") && productUser.selected
-                  ? "active-item"
-                  : ""
-              }`}
+              className={`product-listing ${productUser.hasOwnProperty("selected") && productUser.selected
+                ? "active-item"
+                : ""
+                }`}
               key={index}
               onClick={() => selectUser(id)}
             >
